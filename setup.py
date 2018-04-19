44239a99c13c583fca782e6ec7b3378fc635617a
thodson-usgs <thodson@usgs.gov>
2018-04-18 20:03:32 -0500

    Initial commit; used for WY 2017 report

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qw_reports',
+      version='0.1',
+      description='Tools for preparing water quality reports',
+      url='',
+      author='Timothy Hodson',
+      author_email='thodson@usgs.gov',
+      license='MIT',
+      packages=['qw_reports'],
+      zip_safe=False)
